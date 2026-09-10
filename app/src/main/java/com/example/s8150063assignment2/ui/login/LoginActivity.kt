871130586ecac4574154a0013e6756d1084ec579
Lyzataing <lyza.taing@live.vu.edu.au>
2026-09-10 13:43:11 +1000

    Created login screen UI

--- app/src/main/java/com/example/s8150063assignment2/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/s8150063assignment2/ui/login/LoginActivity.kt
@@ -1,21 +1,22 @@
 package com.example.s8150063assignment2.ui.login
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.s8150063assignment2.R
+import android.content.Intent
+import android.widget.Button
+import com.example.s8150063assignment2.ui.dashboard.DashboardActivity
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
         }
     }
 }
